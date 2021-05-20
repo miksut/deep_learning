fa1e36c55684288d8c1e49854853c131d0e02224
Mike <mi.su@hispeed.ch>
2021-05-20 21:26:47 +0200

    task12: prepared dataset

--- 12_RNN.py
+++ 12_RNN.py
@@ -1,4 +1,5 @@
 import torch
+from torch.utils import data
 import numpy as np
 import os
 from os.path import exists
@@ -7,7 +8,19 @@ import re
 from sklearn.preprocessing import OneHotEncoder
 
 
+# Classes and functions
+# --------------------------------------
+class SequenceDataset(data.Dataset):
+	def __init__(self, X_, T_):
+		assert(X_.shape == T_.shape), "Non-matching dimensions of data and targets"
+		self.X = torch.DoubleTensor(X_)
+		self.T = torch.DoubleTensor(T_)
 
+	def __getitem__(self, idx):
+		return self.X[idx], self.T[idx]
+
+	def __len__(self):
+		return self.X.shape[0]
 
 
 # Downloading the raw dataset
@@ -49,4 +62,29 @@ oneHot_enc = enc.fit_transform(char_arr.reshape(-1,1))
 # dictionaries for lookups: char->OneHot and OneHot->char
 char_dict = {el : oneHot_enc[np.where(char_arr == el)[0][0]] for el in char_arr}
 oneHot_dict = {val.tobytes() : key for key, val in char_dict.items()}
+
+# OneHot-encode text string
+data_enc = np.zeros([len(data_raw), len(char_arr)])
+
+for i in range(len(data_raw)):
+	data_enc[i] = char_dict[data_raw[i]]
+
+
+# sequence encoding: preparing samples
+# --------------------------------------
+seq_length = 20
+n_samples = len(data_raw)-seq_length
+X = np.zeros([n_samples,seq_length,len(char_arr)])
+T = np.zeros([n_samples,seq_length,len(char_arr)])
+
+for i in range(n_samples):
+	X[i] = data_enc[i : i+seq_length]
+	T[i] = data_enc[i+1 : i+1+seq_length]
+
+# instantiating datset and dataloader
+dataset = SequenceDataset(X, T)
+data_loader = data.DataLoader(dataset=dataset)
+
+#print(oneHot_dict[X_torch[-1][-1].numpy().tobytes()])
+
 
